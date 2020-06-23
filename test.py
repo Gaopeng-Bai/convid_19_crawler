d4048457d4e939625f3e087f29505d526b3300eb
Gaopeng-Bai <gaopengbai0121@gmail.com>
2020-06-23 21:29:13 +0200

    new

--- test.py
+++ test.py
@@ -13,6 +13,6 @@ from selenium import webdriver
 
 option = webdriver.ChromeOptions()
 option.add_argument('--headless')
-option.add_argument("--no-sandbox")
+# option.add_argument("--no-sandbox")
 option.add_argument("--disable-dev-shm-usage")
 driver = webdriver.Chrome(options=option)
